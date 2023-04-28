500be5a673c2412c5dcda93d5e15d6b069982020
eugenyan <eugeapedraza@gmail.com>
2023-04-28 20:22:09 -0300

    something special

---
+++ Desafios/Ejercicio2.py
@@ -0,0 +1,17 @@
+
+
+nombre = input("Nombre completo: ")
+edad = int(input("Edad: "))
+estatura = int(input("Estatura: "))
+peso = int(input("Peso: "))
+direccion = input("Direccion: ")
+numero_telefono = input("Numero de telefono: ")
+
+print(f'''
+    Nombre completo: {nombre}
+    Edad: {edad}
+    Estatura: {estatura} cm
+    Peso: {peso} kg
+    Dirección: {direccion}
+    Número de teléfono: {numero_telefono}
+''')
--- listas.py
+++
@@ -1,5 +0,0 @@
-#Listas
-listas=["hola", 5, 12.5, ["adios", 4]]
-
-listas.insert(1, "juan")
-print(listas)
